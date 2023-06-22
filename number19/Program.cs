ebfb05535967d76e04be2c6e58b13296f3cf903b
Liudmila <yaveryvz@mail.ru>
2023-06-22 13:54:09 +0300

    add number19,21,23

---
+++ number19/Program.cs
@@ -0,0 +1,39 @@
+﻿// Напишите программу, которая принимает на вход пятизначное число и проверяет,
+// является ли оно палиндромом
+// 14212 -> нет
+// 12821 -> да
+// 23432 -> да
+
+int num =Prompt("Введите пятизначное число: ");
+Comparison(num);
+
+int Prompt(string message)
+{
+    Console.Write(message);
+    int number =int.Parse(Console.ReadLine()!);
+    return number;
+}
+
+void Comparison(int number)
+{
+    if(number>9999 && number<100000)
+    {
+        if (number/10000 ==number%10)
+        {
+            if (number/1000%10 ==number/10%10)
+            {
+                Console.WriteLine(number + "->да");
+            }
+        }
+        else
+        {
+         Console.WriteLine(number + "-> нет");   
+        }
+    }
+    else
+    {
+      Console.WriteLine("Введено неверное число, введите пятизначное число: ");
+      return;
+    }
+}
+
---
+++ number21/Program.cs
@@ -0,0 +1,20 @@
+﻿// Напишите программу, которая принимает на вход координаты двух точек и 
+// находит расстояние между ними в 3D пространстве
+// A(3,6,8); B(2,1,-7) -> 15,84
+// A(7,-5,0); B(1,-1,9) -> 11,53
+
+int XA = Prompt("Введите координату x точки A: ");
+int YA = Prompt("Введите координату y точки A: ");
+int ZA = Prompt("Введите координату z точки A: ");
+int XB = Prompt("Введите координату x точки B: ");
+int YB = Prompt("Введите координату y точки B: ");
+int ZB = Prompt("Введите координату z точки B: ");
+
+int Prompt(string message)
+{
+    Console.Write(message);
+    int number = int.Parse(Console.ReadLine()!);
+    return number;
+}
+ double AB = Math.Sqrt((XB-XA)*(XB-XA)+(YB-YA)*(YB-YA)+(ZB-ZA)*(ZB-ZA));
+Console.Write($"A({XA},{YA},{ZA}); B({XB},{YB},{ZB} -> {AB})");

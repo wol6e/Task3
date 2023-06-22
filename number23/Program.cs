2ba0e68c9d1c38ce1d0f9054eaf6b9258cee2938
Liudmila <yaveryvz@mail.ru>
2023-06-22 14:22:12 +0300

    add number23

--- number23/Program.cs
+++ number23/Program.cs
@@ -3,3 +3,28 @@
 // 3 -> 1,8,27
 // 5 -> 1,8,27,64,125
 
+int num = Prompt("Введите натуральное число: ");
+Cube (num);
+int Prompt(string message)
+{
+Console.Write(message);
+int number = int.Parse(Console.ReadLine()!);
+return number;
+}
+
+void Cube(int number)
+{
+    if (number > 0)
+    {
+    int i=1;
+      while (i<=number)
+      {
+        Console.Write(i*i*i + " ");
+         i++;
+      }
+    }
+    else
+     {
+       Console.Write("Введено неверное число, введите натуральное число ");
+     }
+}
